6086715bdaea0e09689e0b726792f07a494b7d73
Sergey Popov <popow_sv@ukr.net>
2023-02-18 13:40:39 +0200

    Change class cat

--- src/homework4/oopInheritance/Cat.java
+++ src/homework4/oopInheritance/Cat.java
@@ -10,15 +10,15 @@ public class Cat extends Animal {
     @Override
     public void run(double distance) {
         if (distance < 200) {
-            System.out.println("cat run " + distance + " m.");
+            System.out.println("Cat run " + distance + " m.");
         } else {
-            System.out.println("cat can't run this distance " + distance + " m.");
+            System.out.println("Cat can't run this distance " + distance + " m.");
         }
     }
 
     @Override
     public void swim(double distance) {
-        System.out.println("сat can't swim");
+        System.out.println("Cat can't swim");
     }
 
     public static int getInstanceCount() {
